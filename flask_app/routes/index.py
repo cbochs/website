bd7546e03b834a8e6c4ca2d102bbb3bff3dcc075
calvin <calvin.bochulak@gmail.com>
2019-10-20 23:05:57 -0600

    Add basic landing home page

--- flask_app/routes/index.py
+++ flask_app/routes/index.py
@@ -1,8 +1,15 @@
 from flask import render_template
 
 from flask_app import app
+from flask_app.spotify.api import Spotify
+from flask_app.spotify.credentials import SpotifyClientCredentials
+from flask_app.spotify.oauth import SpotifyOAuth
 
 
 @app.route('/')
 def index():
+    credentials = SpotifyClientCredentials(app=app)
+    token_info = SpotifyOAuth.load_token_info(credentials)
+    spotify = Spotify(token_info, credentials)
+
     return render_template('index.html')

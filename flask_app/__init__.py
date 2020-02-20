77e6db18e8d20e7de7321ce425c1c7763f6e92b3
cbochs <calvin.bochulak@gmail.com>
2020-02-20 16:51:24 -0700

    Add database migration versioning to app

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
-db = PyMongo(app)
 
+mongodb = PyMongo(app)
+mysqldb = SQLAlchemy(app)
+
+migrate_mongodb = Migrate(app, mongodb)
+migrate_mysqldb = Migrate(app, mysqldb)
+
+from flask_app import models
 from flask_app import routes

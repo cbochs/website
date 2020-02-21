5d01c75ce6ceed961d99430e964bd44d603681a1
cbochs <calvin.bochulak@gmail.com>
2020-02-20 20:08:27 -0700

    Add flask-session and change default json_encoder

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_pymongo import PyMongo
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
+from flask_app.utils.jsonencoder import JSONEncoder
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
+app.json_encoder = JSONEncoder
+
+Session(app)
 
 mongodb = PyMongo(app)
 mysqldb = SQLAlchemy(app)

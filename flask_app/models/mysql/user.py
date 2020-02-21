256d3b4b3ec4ac9673f6bc3d0b38dab25abb5a5b
cbochs <calvin.bochulak@gmail.com>
2020-02-20 20:09:36 -0700

    Add user find and authentication to User model

--- flask_app/models/mysql/user.py
+++ flask_app/models/mysql/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+# from sqlalchemy import Column, Integer, String, DateTime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from flask_app import mysqldb as db
@@ -7,15 +7,50 @@ from flask_app import mysqldb as db
 class User(db.Model):
     __tablename__ = 'users'
 
-    id       = Column(Integer, primary_key=True)
-    email    = Column(String(320), unique=True, nullable=False)
-    username = Column(String(80),  unique=True, nullable=False)
-    password = Column(String(128),              nullable=False)
+    id       = db.Column(db.Integer, primary_key=True)
+    email    = db.Column(db.String(320), unique=True, nullable=False)
+    username = db.Column(db.String(80),  unique=True, nullable=False)
+    password = db.Column(db.String(128),              nullable=False)
 
     def __init__(self, email, username, password):
         self.email    = email
         self.username = username
-        self.password = password
+        self.password = generate_password_hash(password, method='sha256')
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            email=self.email,
+            username=self.username)
+
+    @classmethod
+    def find_user(cls, **kwargs):
+        email    = kwargs.get('email')
+        username = kwargs.get('username')
+
+        if email:
+            return cls.query.filter_by(email=email).first()
+
+        if username:
+            return cls.query.filter_by(username=username).first()
+
+        return None
+
+    @classmethod
+    def authenticate(cls, **kwargs):
+        password = kwargs.get('password')
+
+        user = User.find_user(**kwargs)
+        if user is None:
+            return None
+        
+        if not check_password_hash(user.password, password):
+            return None
+
+        return user
+
+    def __repr__(self):
+        return f'<username: {self.username}, email: {self.email}, id: {self.id}>'
 
     # @classmethod
     # def authenticate(cls, **kwargs):

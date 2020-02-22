91d00474bdb9a70a0c05384dd0d9faae03e4c5b8
cbochs <calvin.bochulak@gmail.com>
2020-02-22 14:29:32 -0700

    Clean up response wording and small changes

--- flask_app/routes/authenticate.py
+++ flask_app/routes/authenticate.py
@@ -12,12 +12,12 @@ def register():
     user = User.find_user(id=session.get('user_id'))
     if user:
         app.logger.info(f'User is already registered and logged in {user}')
-        return make_response('User already exists.', 304)
+        return make_response('Already authenticated.', 304)
 
     data = request.get_json()
     user = User.find_user(**data)
 
-    if user is None:
+    if not user:
         user = User(**data)
         mysqldb.session.add(user)
         mysqldb.session.commit()
@@ -27,6 +27,7 @@ def register():
     else:
         app.logger.info(f'User already exists {user}')
         return make_response('User already exists.', 304)
+
 
 @app.route('/api/login', methods=('POST',))
 def login():
@@ -38,7 +39,7 @@ def login():
     data = request.get_json()
     user = User.authenticate(**data)
 
-    if user is None:
+    if not user:
         app.logger.info(f'Failed to authenticate user')
         return make_response('Failed to authenticate.', 401)
     else:
@@ -47,31 +48,29 @@ def login():
         app.logger.info(f'Authenticated user {user}')
         return make_response('Authenticated.', 200)
 
+
 @app.route('/api/logout', methods=('GET',))
 def logout():
     user = User.find_user(id=session.get('user_id'))
-    if user:
-        app.logger.info(f'Logging out user {user}')
-        session.clear()
-    else:
+    if not user:
         app.logger.info(f'User is not logged in')
-    
+        return make_response('Unauthenticated.', 401)
+
+    app.logger.info(f'Logging out user {user}')
+    session.clear()
     return make_response('Logged out.', 200)
+
+
 
 @app.route('/api/user', methods=('GET',))
 def current_user():
     user = User.find_user(id=session.get('user_id'))
-    if user:
-        app.logger.info(user)
-        return make_response(jsonify({
-            'user_id': user.id,
-            'username': user.username,
-            'email': user.email
-        }), 200)
-    else:
-        app.logger.info(f'User is not logged in')
-        return make_response(jsonify({
-            'user_id': None,
-            'username': None,
-            'email': None
-        }), 200)
+    if not user:
+        return make_response('Unauthenticated.', 401)
+
+    app.logger.info(user)
+    return make_response(jsonify({
+        'user_id': user.id,
+        'username': user.username,
+        'email': user.email
+    }), 200)
